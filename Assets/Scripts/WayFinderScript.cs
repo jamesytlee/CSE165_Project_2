a5036faa8bb786d0067c6eb05570e2e0c7a949d9
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-05-09 19:01:05 -0700

    Updated scripts and added wayfinder script

--- Assets/Scripts/WayFinderScript.cs
+++ Assets/Scripts/WayFinderScript.cs
@@ -4,59 +4,11 @@ using UnityEngine;
 
 public class WayFinderScript : MonoBehaviour
 {
-    public Transform currentCheckpoint; // The current checkpoint the object is heading towards
-    public float distanceThreshold = 5f; // The minimum distance required to reach a checkpoint
-    public GameObject[] checkpoints; // The array of all checkpoints in the scene with the "Checkpoint" tag
+    public CheckpointSpawner checkpointManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Find all game objects in the scene with the "Checkpoint" tag
-        checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // Find the nearest checkpoint to the object
-        Transform nearestCheckpoint = GetNearestCheckpoint();
-
-        // If the nearest checkpoint is different from the current checkpoint
-        if (nearestCheckpoint != currentCheckpoint)
-        {
-            // Set the nearest checkpoint as the current checkpoint
-            currentCheckpoint = nearestCheckpoint;
-            Debug.Log("Heading towards checkpoint " + currentCheckpoint.name);
-        }
-
-        // Check if the object has reached the current checkpoint
-        float distanceToCheckpoint = Vector3.Distance(transform.position, currentCheckpoint.position);
-        if (distanceToCheckpoint < distanceThreshold)
-        {
-            Debug.Log("Reached checkpoint " + currentCheckpoint.name);
-
-            // Set the next checkpoint as the current checkpoint
-            int nextCheckpointIndex = (System.Array.IndexOf(checkpoints, currentCheckpoint.gameObject) + 1) % checkpoints.Length;
-            currentCheckpoint = checkpoints[nextCheckpointIndex].transform;
-            Debug.Log("Heading towards checkpoint " + currentCheckpoint.name);
-        }
-    }
-    Transform GetNearestCheckpoint()
-    {
-        Transform nearestCheckpoint = null;
-        float minDistance = Mathf.Infinity;
-
-        // Find the nearest checkpoint to the object
-        foreach (GameObject checkpoint in checkpoints)
-        {
-            float distance = Vector3.Distance(transform.position, checkpoint.transform.position);
-            if (distance < minDistance)
-            {
-                nearestCheckpoint = checkpoint.transform;
-                minDistance = distance;
-            }
-        }
-
-        return nearestCheckpoint;
+        Transform target = checkpointManager.PeekCheckpoint().transform;
+        transform.LookAt(target);
     }
 }
